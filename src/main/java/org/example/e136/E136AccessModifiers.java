87c06c01fdef58ed4cf70746f0b78e540987622c
jbuchanan89 <jen.rumingan@gmail.com>
2024-09-03 20:08:14 -0400

    HW update

--- src/main/java/org/example/e136/E136AccessModifiers.java
+++ src/main/java/org/example/e136/E136AccessModifiers.java
@@ -2,9 +2,29 @@ package org.example.e136;
 
 public class E136AccessModifiers {
 
+        private static void privateMethod(){
+            System.out.println("This is a private method");
+        }
+
+        static void defaultMethod(){
+            System.out.println("This is a default method");
+        }
+
+        protected void protectedMethod(){
+            System.out.println("This is a protected method");
+        }
+
+        public static void publicMethod(){
+            System.out.println("This is a public method");
+        }
 
     public static void main(String[] args) {
+        E136AccessModifiers prints = new E136AccessModifiers();
 
+        prints.privateMethod();
+        prints.defaultMethod();
+        prints.protectedMethod();
+        prints.publicMethod();
     }
 }
 
--- src/main/java/org/example/e137/E137AccessModifiers.java
+++ src/main/java/org/example/e137/E137AccessModifiers.java
@@ -1,8 +1,18 @@
 package org.example.e137;
 
 public class E137AccessModifiers {
-    public static void main(String[] args) {
 
+    static void schoolInformation(String name, String city, String schoolName, int batchNumber){
+        System.out.println("My name is " + name + " and I live in " + city + ". I study at " + schoolName + " in batch " + batchNumber);
+    }
+    public static void main(String[] args) {
+        String name;
+        String city;
+        String schoolName;
+        int batchNumber;
+
+        E137AccessModifiers schoolInfo = new E137AccessModifiers();
+        schoolInfo.schoolInformation("Jennifer", "Falls Church", "Syntax", 20);
     }
 }
 
